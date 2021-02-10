0340fd4affc8c02efa008c02eabcccf15e7a4747
Priti Shaw <pritishaw0103@gmail.com>
2021-02-10 17:54:18 +0530

    Add SOS screen

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ import {
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Profile from './components/Profile'
+import SOS from "./components/SOS";
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
+          <Route path="/sos" exact component={SOS}/> 
           <Route path="/profile" exact component={Profile}/>
           <Route path="/" exact component={Home}/>
+          
           <Route path="*">
             <h1>Not found</h1>
           </Route>
