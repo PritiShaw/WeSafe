e795d297b72b5f8f10559cc9c2b4ab25b9413ad9
Priti Shaw <pritishaw0103@gmail.com>
2021-02-10 19:03:59 +0530

    Add nearby emergency screen

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import {
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Profile from './components/Profile'
-import SOS from "./components/SOS";
+import SOS from './components/SOS'
+import Nearby from './components/NearbyEmergency'
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
+          <Route path="/nearby" exact component={Nearby}/>
           <Route path="/sos" exact component={SOS}/> 
           <Route path="/profile" exact component={Profile}/>
           <Route path="/" exact component={Home}/>
-          
           <Route path="*">
             <h1>Not found</h1>
           </Route>
